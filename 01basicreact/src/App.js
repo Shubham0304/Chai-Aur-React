8708b3a27ccc754d1a4400dd38099500847cf489
Shubham0304 <shubhamjoshi0304@gmail.com>
2025-11-04 06:30:50 +0530

    ReactJS tutorial

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Chai and React | Shubham </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Chai aur React With Vite | Shubham </h1>
+  )
+}
+
+export default App
